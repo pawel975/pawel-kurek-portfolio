9ce1b923d2edbb70b9c6fe70e9497ed2ca6d3c69
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-04-17 01:39:30 +0200

    gsap initialize

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -1,7 +1,41 @@
 import "./LandingPage.scss";
 import { FaLaptopCode } from 'react-icons/fa';
+import { useEffect } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/src/ScrollTrigger";
 
 const LandingPage = () => {
+
+    useEffect(() => {
+        gsap.registerPlugin(ScrollTrigger);
+        const tl = gsap.timeline();
+        const heading = document.querySelector(".landing-page__welcome-heading")
+
+        tl.from(heading.children, {
+            scrollTrigger: {
+                trigger: heading,
+                start:"bottom top",
+                end: "+200",
+                markers: {
+                    startColor: 'red',
+                    endColor: "salmon"
+                },
+                toggleActions: 'restart'
+            },
+            duration: 1, 
+            opacity:0, 
+            x: '-200',
+            // stagger: 0.3,
+        })
+
+        // tl.from('.landing-page__logo', {
+        //     duration: 1, 
+        //     opacity:0, 
+        //     y: '-200',
+        // })
+
+    }, [])
+
     return (
         <section id="landing-page">
             <header className="landing-page__welcome-heading">
