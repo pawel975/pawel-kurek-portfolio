d163d8a4f349e47d19aa576a37d0ad573ae1a4bd
Pawel  Kurek <pawel.kurek975@gmail.com>
2022-06-02 21:28:44 +0200

    update projects list

--- src/helpers/projects.js
+++ src/helpers/projects.js
@@ -4,14 +4,44 @@ import fyMotivation from '../assets/img/fy-motivation.png';
 import fyMeasures from '../assets/img/fy-measures.png';
 import waHome from '../assets/img/wa-home.png';
 import waModal from '../assets/img/wa-modal.png';
+import paHome from '../assets/img/pa-home.png';
+import paModalSettings from '../assets/img/pa-modal-settings.png';
+import paRules from '../assets/img/pa-rules.png';
+import paRun from '../assets/img/pa-run.png';
+import paStatistics from '../assets/img/pa-statistics.png';
+import paThemeChanged from '../assets/img/pa-theme-changed.png';
+import paAppearance from '../assets/img/pa-appearance.png';
 import {AiFillHtml5} from 'react-icons/ai';
-import {SiJavascript, SiJest, SiRedux} from 'react-icons/si';
+import {SiJavascript, SiJest, SiRedux, SiTestinglibrary} from 'react-icons/si';
 import {GrReactjs} from 'react-icons/gr';
 import {DiSass} from 'react-icons/di';
 
 const projects = [
     {
         id: 1,
+        name: 'PomodoroApp',
+        description: "Improve your learning workflow with classical pomodoro method, which divide time onto learning and break session to maximize focus",
+        images: [
+            paHome,
+            paRun,
+            paModalSettings,
+            paStatistics,
+            paRules,
+            paAppearance,
+            paThemeChanged
+        ],
+        techStack: [
+            <GrReactjs/>,
+            <SiTestinglibrary/>,
+            <AiFillHtml5/>,
+            <DiSass/>,
+        ],
+        githubLink: 'https://github.com/pawel975/pomodoro-app.git',
+        liveVersionLink: 'https://pomodoro-app-blush.vercel.app/',
+
+    },
+    {
+        id: 2,
         name: 'FitYou',
         description: "Track your daily diet based on goals and measures",
         images: [
@@ -31,7 +61,7 @@ const projects = [
 
     },
     {
-        id: 1,  
+        id: 3,  
         name: 'WeatherApp',
         description: "Weather app with hours and days forecast, get info about temperature, weather and more details stuff.",
         images: [
