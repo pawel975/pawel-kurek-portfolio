d20a53b1bda6be1b4e54e5adac29268ab34f0f10
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-05-25 18:27:39 +0200

    migration projects to data folder

--- src/Components/ProjectsPage/ProjectsPage.js
+++ src/Components/ProjectsPage/ProjectsPage.js
@@ -1,52 +1,49 @@
-import './ProjectsPage.scss'
-import projects from '../../helpers/projects';
-import Project from '../Project/Project';
-import { useEffect } from 'react';
-import gsap from 'gsap';
+import "./ProjectsPage.scss";
+import projects from "../../data/projects";
+import Project from "../Project/Project";
+import { useEffect } from "react";
+import gsap from "gsap";
 
-const allProjects = projects.map(project => (
-    <>
-        <Project 
-            key={project.id} 
-            id={project.id}
-            name={project.name}
-            description={project.description}
-            images={project.images}
-            techStack={project.techStack}
-            githubLink={project.githubLink}
-            liveVersionLink={project.liveVersionLink}
-        />
-    </>
-))
+const allProjects = projects.map((project) => (
+  <>
+    <Project
+      key={project.id}
+      id={project.id}
+      name={project.name}
+      description={project.description}
+      images={project.images}
+      techStack={project.techStack}
+      githubLink={project.githubLink}
+      liveVersionLink={project.liveVersionLink}
+    />
+  </>
+));
 
 const ProjectsPage = () => {
+  useEffect(() => {
+    const projectsPageEls = document.querySelector("#projects-page").children;
 
-    useEffect(()=> {
+    [...projectsPageEls].forEach((element) => {
+      gsap.from(element, {
+        scrollTrigger: {
+          trigger: element,
+          toggleActions: "play none none none",
+        },
+        duration: 0.5,
+        opacity: 0,
+        x: "-50",
+      });
+    });
+  });
 
-        const projectsPageEls = document.querySelector("#projects-page").children;
-
-        [...projectsPageEls].forEach(element => {
-            gsap.from(element, {
-                scrollTrigger: {
-                    trigger: element,
-                    toggleActions: "play none none none",
-                },
-                duration: 0.5,
-                opacity: 0,
-                x: '-50',
-            })
-        })
-
-    })
-
-    return(
-        <section id="projects-page">
-            <header className='subtitle'>
-                <h2>Selected personal projects</h2>
-            </header>
-            {allProjects}
-        </section>
-    )
-} 
+  return (
+    <section id="projects-page">
+      <header className="subtitle">
+        <h2>Selected personal projects</h2>
+      </header>
+      {allProjects}
+    </section>
+  );
+};
 
 export default ProjectsPage;
